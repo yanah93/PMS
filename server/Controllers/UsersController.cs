a63ba7ad524f5ecfcbcafd1552d6bcaef8390b84
yana93 <maryanah.faizan@gmail.com>
2022-05-05 12:56:32 +0800

    set up base

---
+++ server/Controllers/UsersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+    }
+}
