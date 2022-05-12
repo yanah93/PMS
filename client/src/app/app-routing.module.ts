481a5aff9c97a64410e29897735fc75507b7de74
yana93 <maryanah.faizan@gmail.com>
2022-05-12 09:18:40 +0800

    base folders

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: () => import('./login/login.module').then(x => x.LoginModule) },
+  { path: 'application', loadChildren: () => import('./application/application.module').then(x => x.ApplicationModule)}
 
 ];
 @NgModule({
